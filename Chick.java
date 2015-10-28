a6bea30b99b08a8a779967a08ecbce4bb3f20d21
darrenapcs <lgytuan@gmail.com>
2015-10-28 10:33:32 -0700

    done

--- Chick.java
+++ Chick.java
@@ -2,19 +2,33 @@ class Chick implements Animal
 {     
 	private String myType;
 	private String mySound;
-	public Chick(String type, String sound)
+	private String mysound1;
+	
+
+	public Chick(String type, String sound, String sound1)
 	{
 		myType = type;
-		mySound = sound;  
+		mySound = sound;
+		mysound1 = sound1; 
 	}
 	public Chick()
 	{
 		myType = "unkown";
 		mySound = "unkown";
+		mysound1 = "unkown";
+		
+
 
 	}
 
 
-	public String getSound(){return mySound;}
+	public String getSound()
+	{
+	if(Math.random()*1 > 0.5)
+	     return mySound;
+    else
+    	return mysound1;
+}
+	
 	public String getType(){return myType;}
 }
--- Cow.java
+++ Cow.java
@@ -1,15 +1,19 @@
 class Cow implements Animal 
 {     
     private String myType;     
-     private String mySound;      
+     private String mySound;  
+    
      public Cow(String type, String sound)     {         
          myType = type;         
-         mySound = sound;     
+         mySound = sound;
+             
      }     
      public Cow()     {         
          myType = "unknown";         
-         mySound = "unknown";     
+         mySound = "unknown"; 
+         
      }      
      public String getSound(){return mySound;}     
      public String getType(){return myType;} 
+    
 }
