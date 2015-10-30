50d89541800c4755b7ef58730e25a35a5bbda81d
darrenapcs <lgytuan@gmail.com>
2015-10-30 10:08:01 -0700

    re

--- NamedCow.java
+++ NamedCow.java
@@ -1,11 +1,24 @@
 class NamedCow extends Cow
 {
     private String myName;
+    private String myType;     
+    private String mySound;
 
    public NamedCow(String type, String name, String sound)
     {
     	myName = name;
+    	myType = type;         
+        mySound = sound;
     }
+     public NamedCow()   
+    {         
+         myType = "cow";         
+         mySound = "moo";
+         myName = "Elsie";
+         
+     }    
    
     public String getName(){return myName;}
+    public String getSound(){return mySound;}     
+    public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,8 +1,8 @@
 
 public void setup()
 {
-	Cow c = new Cow("cow", "moo");   
-    System.out.println(c.getType() + " goes " + c.getSound());  
+ 
+    
     Farm a = new Farm();
     a.animalSounds();
     //System.out.println(a.getType() + " goes " + a.getSound());
